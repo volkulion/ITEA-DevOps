43af9c0d1207eeabf50b8876e55cbdb3f41e619e
Taras Kolodchyn <taras.kolodchyn@me.com>
2019-06-09 13:21:29 +0300

    terraform lab8

--- Terraform/Azure/Lab8/lab8.tf
+++ Terraform/Azure/Lab8/lab8.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
-  location = "${var.location}"
+  location = var.location
 }
 
 resource "azurerm_app_service_plan" "main" {
   name                = "${var.prefix}-asp"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   kind                = "Linux"
   reserved            = true
 
@@ -18,9 +18,9 @@ resource "azurerm_app_service_plan" "main" {
 
 resource "azurerm_app_service" "main" {
   name                = "${var.prefix}-appservice"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
 
   site_config {
     app_command_line = ""
@@ -32,3 +32,4 @@ resource "azurerm_app_service" "main" {
     "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
   }
 }
+
--- Terraform/Azure/Lab8/variables.tf
+++ Terraform/Azure/Lab8/variables.tf
@@ -1,9 +1,10 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
-  default ="itea"
+  default     = "itea"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  default = "westeurope"
+  default     = "westeurope"
 }
+
