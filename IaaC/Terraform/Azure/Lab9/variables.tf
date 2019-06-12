ffdc04ef3dd8213c6ecfc79ed39115710dcc3016
Volodymyr Hrushovenko <volkulion@gmail.com>
2019-06-12 21:16:23 +0300

    Update variables.tf

--- IaaC/Terraform/Azure/Lab9/variables.tf
+++ IaaC/Terraform/Azure/Lab9/variables.tf
@@ -1,6 +1,6 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
-  default     = "volkulion123"
+  default     = "volkulion123123123"
 }
 
 variable "location" {
